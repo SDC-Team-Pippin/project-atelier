f675397394e186c2b76ba92abba5b2d0568e5180
Alec <alecech1994@gmail.com>
2021-06-25 15:51:05 -0700

    Wrote post routes and put routes for QA service

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(morgan('dev'));
 app.get('/qa/questions', (req, res) => {
   const productId = req.query.product_id;
   const count = req.query.count || 5;
-  const query = 'SELECT q.id AS question_id, q.body AS question_body, q.date_written AS question_date, q.asker_name, q.helpful AS question_helpfulness, q.reported, (SELECT json_agg(ans) FROM (SELECT an.id, an.body AS body, an.date_written AS date, an.answerer_name, an.helpful AS helpfulness, (SELECT json_agg(pho) FROM (SELECT photos.answer_id AS id, photos.body AS url FROM photos WHERE photos.answer_id = an.id) pho) AS photos FROM answers AS an WHERE q.id = an.question_id AND an.reported = FALSE) ans) AS answers FROM questions AS q WHERE q.product_id = $1 AND q.reported = FALSE ORDER BY q.helpful DESC LIMIT $2;';
+  const query = 'SELECT q.id AS question_id, q.body AS question_body, q.date_written AS question_date, q.asker_name, q.helpful AS question_helpfulness, q.reported, (SELECT json_agg(ans) FROM (SELECT an.id, an.body AS body, an.date_written AS date, an.answerer_name, an.helpful AS helpfulness, (SELECT json_agg(pho) FROM (SELECT photos.answer_id AS id, photos.url FROM photos WHERE photos.answer_id = an.id) pho) AS photos FROM answers AS an WHERE q.id = an.question_id AND an.reported = FALSE) ans) AS answers FROM questions AS q WHERE q.product_id = $1 AND q.reported = FALSE ORDER BY q.helpful DESC LIMIT $2;';
   const queryParams = [productId, count];
   client
     .query(query, queryParams)
@@ -48,7 +48,7 @@ app.get('/qa/questions', (req, res) => {
 app.get('/qa/answers', (req, res) => {
   const questionId = req.query.question_id;
   const count = req.query.count || 5;
-  const query = 'SELECT an.id, an.body AS body, an.date_written AS date, an.answerer_name, an.helpful AS helpfulness, (SELECT json_agg(pho) FROM (SELECT photos.answer_id AS id, photos.body AS url FROM photos WHERE photos.answer_id = an.id) pho) AS photos FROM answers AS an WHERE an.question_id = $1 AND an.reported = FALSE ORDER BY an.helpful DESC LIMIT $2;';
+  const query = 'SELECT an.id, an.body AS body, an.date_written AS date, an.answerer_name, an.helpful AS helpfulness, (SELECT json_agg(pho) FROM (SELECT photos.answer_id AS id, photos.url FROM photos WHERE photos.answer_id = an.id) pho) AS photos FROM answers AS an WHERE an.question_id = $1 AND an.reported = FALSE ORDER BY an.helpful DESC LIMIT $2;';
   const queryParams = [questionId, count];
   client
     .query(query, queryParams)
@@ -67,42 +67,80 @@ app.get('/qa/answers', (req, res) => {
 app.post('/qa/questions', (req, res) => {
   const productId = req.query.product_id;
   const { body, name, email } = req.body;
-  const unixDate = Math.floor(Date.now() / 1000);
-  const query = 'INSERT INTO questions(product_id, body, date_written, asker_name, asker_email) VALUES ($1, $2, TO_TIMESTAMP($3), $4, $5) RETURNING *';
-  const queryParams = [productId, body, unixDate, name, email];
+  const query = 'INSERT INTO questions(product_id, body, asker_name, asker_email) VALUES ($1, $2, $3, $4);';
+  const queryParams = [productId, body, name, email];
   client
     .query(query, queryParams)
     .then(() => (
-      res.status(200).send('Success')
+      res.status(201).send('Success')
     ))
     .catch((err) => (
-      res.status(404).send(err)
+      res.status(400).send(err)
     ));
 });
 
 app.post('/qa/answers', (req, res) => {
-  console.log(req.body);
-  res.status(200).send('hello there');
+  const questionId = req.query.question_id;
+  const {
+    body, name, email, photos,
+  } = req.body;
+  const query = 'INSERT INTO answers(question_id, body, answerer_name, answerer_email) VALUES ($1, $2, $3, $4);';
+  const queryParams = [questionId, body, name, email];
+  client
+    .query(query, queryParams)
+    .catch((err) => (
+      res.status(400).send(err)
+    ));
+  // const queryParams2 = [photos];
 });
 
 app.put('/qa/question/helpful', (req, res) => {
-  console.log(req.query);
-  res.status(200).send('hello there');
+  const questionId = req.query.question_id;
+  const query = 'UPDATE questions SET helpful = helpful + 1 WHERE id = $1;';
+  const queryParams = [questionId];
+  console.log(questionId);
+  client
+    .query(query, queryParams)
+    .then(() => res.status(204).send())
+    .catch((err) => (
+      res.status(400).send(err)
+    ));
 });
 
 app.put('/qa/answer/helpful', (req, res) => {
-  console.log(req.query);
-  res.status(200).send('hello there');
+  const answerId = req.query.answer_id;
+  const query = 'UPDATE answers SET helpful = helpful + 1 WHERE id = $1;';
+  const queryParams = [answerId];
+  client
+    .query(query, queryParams)
+    .then(() => res.status(204).send())
+    .catch((err) => (
+      res.status(400).send(err)
+    ));
 });
 
 app.put('/qa/question/report', (req, res) => {
-  console.log(req.query);
-  res.status(200).send('hello there');
+  const questionId = req.query.question_id;
+  const query = 'UPDATE questions SET reported = TRUE WHERE id = $1;';
+  const queryParams = [questionId];
+  client
+    .query(query, queryParams)
+    .then(() => res.status(204).send())
+    .catch((err) => (
+      res.status(400).send(err)
+    ));
 });
 
 app.put('/qa/answer/report', (req, res) => {
-  console.log(req.query);
-  res.status(200).send('hello there');
+  const answerId = req.query.answer_id;
+  const query = 'UPDATE answers SET reported = TRUE WHERE id = $1;';
+  const queryParams = [answerId];
+  client
+    .query(query, queryParams)
+    .then(() => res.status(204).send())
+    .catch((err) => (
+      res.status(400).send(err)
+    ));
 });
 
 app.listen(port, () => {
